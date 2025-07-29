625f5800725b21dbe76cc5e89c9931deacf50ffd
xana-rahmani <xanarahmani@gmail.com>
2025-07-29 19:54:35 +0330

    Fixed WTRX

--- contracts/helpers/WETH9_21Inches.sol
+++ contracts/helpers/WETH9_21Inches.sol
@@ -25,22 +25,23 @@ contract WETH9_21Inches {
     }
     function deposit() public payable {
         balanceOf[msg.sender] += msg.value;
-        Deposit(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
     }
     function withdraw(uint wad) public {
-        require(balanceOf[msg.sender] >= wad);
+        require(balanceOf[msg.sender] >= wad, "WTRX: Insufficient balance");
         balanceOf[msg.sender] -= wad;
-        msg.sender.transfer(wad);
-        Withdrawal(msg.sender, wad);
+        (bool success, ) = msg.sender.call{value: wad}("");
+        require(success, "WTRX: TRX transfer failed");
+        emit Withdrawal(msg.sender, wad);
     }
 
     function totalSupply() public view returns (uint) {
-        return this.balance;
+        return address(this).balance;
     }
 
     function approve(address guy, uint wad) public returns (bool) {
         allowance[msg.sender][guy] = wad;
-        Approval(msg.sender, guy, wad);
+        emit Approval(msg.sender, guy, wad);
         return true;
     }
 
@@ -52,17 +53,16 @@ contract WETH9_21Inches {
     public
     returns (bool)
     {
-        require(balanceOf[src] >= wad);
-
-        if (src != msg.sender && allowance[src][msg.sender] != uint(-1)) {
-            require(allowance[src][msg.sender] >= wad);
+        require(balanceOf[src] >= wad, "WTRX: transfer amount exceeds balance");
+        if (src != msg.sender && allowance[src][msg.sender] != type(uint256).max) {
+            require(allowance[src][msg.sender] >= wad, "WTRX: transfer amount exceeds allowance");
             allowance[src][msg.sender] -= wad;
         }
 
         balanceOf[src] -= wad;
         balanceOf[dst] += wad;
 
-        Transfer(src, dst, wad);
+        emit Transfer(src, dst, wad);
 
         return true;
     }
